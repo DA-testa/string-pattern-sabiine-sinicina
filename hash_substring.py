326ca0c70a1edf779b6bce8bd2be4590bd3c1fb9
sabiine-sinicina <124919082+sabiine-sinicina@users.noreply.github.com>
2023-04-13 16:04:47 +0000

    test4

--- hash_substring.py
+++ hash_substring.py
@@ -4,8 +4,8 @@ def read_input():
     input_type = input()
 
     if input_type == "F":
-        file = "tests/06"
-        with open(file) as f:
+        file_name = "tests/06"
+        with open(file_name) as f:
             text1 = f.readlines()
             pattern = text1[0].strip()
             text = text1[1].strip()
